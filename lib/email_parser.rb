9df5d89049d7f6269d28d079d3b480907a26d3fb
Ariel <arielord@buffalo.edu>
2018-12-19 00:17:33 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
   
   def parse
-    emails = self.emails.split(/\s|,|,\s/)
+    emails = self.emails.split(/\s|,\s/)
     emails = emails.collect {|email| email.strip}
     emails.uniq
   end
